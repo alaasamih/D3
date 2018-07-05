fe463c2d6abb8066ca10d9965ff59d7ee69d4055
alaasamih <alaa.samih@hotmail.com>
2018-07-05 15:00:00 +0300

    4.5.7

--- src/Project/D3/code/Properties/AssemblyInfo.cs
+++ src/Project/D3/code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sitecore.Demo.Group")]
+[assembly: AssemblyTitle("Sitecore.Demo.D3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sitecore.Demo.Group")]
+[assembly: AssemblyProduct("Sitecore.Demo.D3")]
 [assembly: AssemblyCopyright("Copyright © Sitecore 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
