f4b0658638b90d06172fb75077876f50ca707ba0
alaasamih <alaa.samih@hotmail.com>
2018-07-10 12:40:59 +0300

    10/7 features

--- src/Feature/Identity/code/Repositories/IdentityRepository.cs
+++ src/Feature/Identity/code/Repositories/IdentityRepository.cs
@@ -1,17 +1,17 @@
-﻿//namespace Sitecore.Feature.Identity.Repositories
-//{
-//  using System;
-//  using Sitecore.Data.Items;
-//  using Sitecore.Foundation.SitecoreExtensions.Extensions;
+﻿namespace Sitecore.Feature.Identity.Repositories
+{
+  using System;
+  using Sitecore.Data.Items;
+  using Sitecore.Foundation.SitecoreExtensions.Extensions;
 
-//  public static class IdentityRepository
-//  {
-//    public static Item Get([NotNull] Item contextItem)
-//    {
-//      if (contextItem == null)
-//        throw new ArgumentNullException(nameof(contextItem));
+  public static class IdentityRepository
+  {
+    public static Item Get([NotNull] Item contextItem)
+    {
+      if (contextItem == null)
+        throw new ArgumentNullException(nameof(contextItem));
 
-//      return contextItem.GetAncestorOrSelfOfTemplate(Templates.Identity.ID) ?? Context.Site.GetContextItem(Templates.Identity.ID);
-//    }
-//  }
-//}
+      return contextItem.GetAncestorOrSelfOfTemplate(Templates.Identity.ID) ?? Context.Site.GetContextItem(Templates.Identity.ID);
+    }
+  }
+}
